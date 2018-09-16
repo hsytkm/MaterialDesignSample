ea5f6be86e54d425b8a1eebfe25c130ce09aa960
hsytkm <t_hosoya1214@yahoo.co.jp>
2018-09-16 13:42:53 +0900

    プロジェクト ファイルを追加します。

---
+++ MaterialDesignSample.MainModule/MainModule.cs
@@ -0,0 +1,26 @@
+﻿using MaterialDesignSample.MainModule.Views;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using Autofac;
+using Prism.Autofac;
+
+namespace MaterialDesignSample.MainModule
+{
+    public class MainModule : IModule
+    {
+        private IRegionManager _regionManager;
+        private ContainerBuilder _builder;
+
+        public MainModule(ContainerBuilder builder, IRegionManager regionManager)
+        {
+            _builder = builder;
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
+        }
+    }
+}
